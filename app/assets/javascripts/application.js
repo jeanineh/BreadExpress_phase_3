908e62e5c5c6704b5fb2b374f0cf917c65b8165a
Jeanine Huang <jeanineh@andrew.cmu.edu>
2015-03-29 10:29:34 -0700

    pretty home page with sidebar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,7 @@ $(document).ready(function(){
      $('.slider').slider({full_width: true});
      $(".dropdown-button").dropdown({ constrain_width: false });
      $('select').material_select();
+     $(".button-collapse").sideNav();
      $('.collapsible').collapsible({
       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
